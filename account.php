c46c7820825f322c66750defbb78fe3ad4d98d22
jcklerk <jklerk3376@student.landstede.nl>
2021-03-02 10:12:32 +0100

    account test

--- account.php
+++ account.php
@@ -1,1 +1,8 @@
 <?php
+require "include/database.php";
+$getusers = $pdo->prepare("SELECT * FROM `user`");
+$getusers->execute();
+
+foreach ($getusers as $user) {
+  echo $user['username'] . "<br>";
+}

9f4d1f3bb6427975c3921433fea427a37b019160
Julpower31 <jkragt@outlook.com>
2021-03-02 11:35:37 +0100

    fixed nav

--- include/nav.php
+++ include/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bc-gray-black">
+<nav class="navbar navbar-expand-lg bc-gray-black fixed-top">
     <div class="container-fluid">
         <a class="navbar-brand c-yellow" href="#">Navbar w/ text</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">

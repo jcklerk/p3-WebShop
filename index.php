889f416f3461d01923867b2270b89d542853fcd0
Julpower31 <jkragt@outlook.com>
2021-03-02 09:48:36 +0100

    nav (not done)

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+require "include/nav.php";
+?>
+<div style="width: 1200px; height: 300px; background: linear-gradient(90deg, #ae8625, #f7ef8a 30%, #d2ac47 80%, #edc967 95%); ">
+</div>
+<div style="width: 300px; height: 300px; background-color: #cd071e">
+</div>
+<div style="width: 300px; height: 300px; background-color: #000000">
+</div>
+<div style="width: 300px; height: 300px; background-color: #D3A13B">
+</div>

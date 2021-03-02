b7704b9e6725ce87c1062b9c621c3d6eec65cb6b
Julpower31 <jkragt@outlook.com>
2021-03-02 12:01:21 +0100

    nearly homepage done

--- index.php
+++ index.php
@@ -7,15 +7,15 @@ require "include/nav.php";
     <div class="parallax img-home d-flex align-items-center justify-content-center">
 
         <div class="text-center">
-            <h1 class="c-red">Wok & Roll</h1>
-            <h1 class="c-white">Woksupplies</h1>
-            <h1 class="c-yellow">Authentic Asian food</h1>
+            <h1 class="c-red fos-1">Wok & Roll</h1>
+            <h1 class="c-white fos-2">Woksupplies</h1>
+            <h1 class="c-yellow fos-2">Authentic Asian food</h1>
         </div>
     </div>
 
 
     <div >
-    <div class="text-center ">
+    <div class="text-center p-1">
         <h2 class="c-red">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, deleniti hic ipsa laborum laudantium veritatis? Eaque, minus praesentium. Aliquam animi beatae ea illo modi nesciunt, porro quaerat quos repellat sequi!</h2>
     </div>
 

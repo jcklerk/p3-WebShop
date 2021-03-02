babb56891776ae0de4e5740bb45c23fbf52b5690
milanbakker <73930747+milanbakker@users.noreply.github.com>
2021-03-02 11:43:21 +0100

    background color

--- index.php
+++ index.php
@@ -2,36 +2,26 @@
 require "include/config.php";
 require "include/nav.php";
 ?>
-
+<body class="bc-gray-black">
 
     <div class="parallax img-home d-flex align-items-center justify-content-center">
 
-        <div class="c-red text-center">
-            <h1>TEST</h1>
+        <div class="text-center">
+            <h1 class="c-red">Wok & Roll</h1>
+            <h1 class="c-white">Woksupplies</h1>
+            <h1 class="c-yellow">Authentic Asian food</h1>
         </div>
     </div>
-<h1>
-   j<br>
-    j<br>
-    j<br>
-    j<br>
-    j<br>
-    j<br>
-    j<br>
-    j<br>
-    j<br>
-    j<br>
-    j<br>    j<br>
-    j<br>
-    j<br>    j<br>    j<br>    j<br>    j<br>    j<br>    j<br>    j<br>
-
-</h1>
 
 
+    <div >
+    <div class="text-center ">
+        <h2 class="c-red">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, deleniti hic ipsa laborum laudantium veritatis? Eaque, minus praesentium. Aliquam animi beatae ea illo modi nesciunt, porro quaerat quos repellat sequi!</h2>
+    </div>
 
+    </div>
 
-
-
+</body>
 
 <?php
 require "include/footer.php";

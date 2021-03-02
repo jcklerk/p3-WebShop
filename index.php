15fd43047169874cd7e47916c31188781d256472
jcklerk <jklerk3376@student.landstede.nl>
2021-03-02 11:24:56 +0100

    d-flex miste....

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require "include/nav.php";
 ?>
 
 
-    <div class="parallax-home align-items-center justify-content-center">
+    <div class="parallax img-home d-flex align-items-center justify-content-center">
 
         <div class="c-red text-center">
             <h1>TEST</h1>

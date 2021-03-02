9be0ad2d1588b6dfe6644e455bc4d41d349a5ea8
milanbakker <73930747+milanbakker@users.noreply.github.com>
2021-03-02 11:22:02 +0100

    test

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 require "include/config.php";
 require "include/nav.php";
 ?>
+
+
+    <div class="parallax-home align-items-center justify-content-center">
+
+        <div class="c-red text-center">
+            <h1>TEST</h1>
+        </div>
+    </div>
 <h1>
    j<br>
     j<br>

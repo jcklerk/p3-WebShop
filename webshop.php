f139268b159b78d4d82102133d0dd7bac93657a5
milanbakker <73930747+milanbakker@users.noreply.github.com>
2021-03-03 13:53:56 +0100

    webshop pagina aangepast

--- webshop.php
+++ webshop.php
@@ -1,5 +1,39 @@
 <?php
 require "include/config.php";
 require "include/nav.php";
+?>
+
+    <body class="bc-gray-black">
+
+
+
+    <div class="parallax-halfsize img-chinesefood d-flex align-items-center justify-content-center">
+
+        <div class="text-center">
+            <h1 class="fos-2">Chinese food</h1>
+
+        </div>
+    </div>
+    <div class="parallax-halfsize img-japanesefood d-flex align-items-center justify-content-center">
+
+        <div class="text-center">
+            <h1 class="fos-2">Japanese food</h1>
+
+        </div>
+    </div>
+    <div class="parallax-halfsize img-koreanfood d-flex align-items-center justify-content-center">
+
+        <div class="text-center">
+            <h1 class="fos-2">Korean food</h1>
+
+        </div>
+    </div>
+
+
+
+
+    </body>
+
+<?php
 require "include/footer.php";
 ?>

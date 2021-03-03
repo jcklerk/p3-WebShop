99bd831b5f31b00923f7239eeffc97d8e8aeb4d4
StephanBear <Stephandebeer@xs4all.nl>
2021-03-03 15:23:34 +0100

    WORKSHOP POG

--- workshop.php
+++ workshop.php
@@ -1,2 +1,39 @@
-hallo
 <?php
+require "include/config.php";
+require "include/nav.php";
+?>
+
+    <body class="bc-gray-black">
+
+
+
+    <div class="parallax-halfheight img-chinesefood d-flex align-items-center justify-content-center">
+
+        <div class="text-center">
+            <h1 class="fos-2">Chinese food</h1>
+
+        </div>
+    </div>
+    <div class="parallax-halfheight img-japanesefood d-flex align-items-center justify-content-center">
+
+        <div class="text-center">
+            <h1 class="fos-2">Japanese food</h1>
+
+        </div>
+    </div>
+    <div class="parallax-halfheight img-koreanfood d-flex align-items-center justify-content-center">
+
+        <div class="text-center">
+            <h1 class="fos-2">Korean food</h1>
+
+        </div>
+    </div>
+
+
+
+
+    </body>
+
+<?php
+require "include/footer.php";
+?>

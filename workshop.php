d12322adf086d1e766ff87ccbde81d20df4ca3c8
jcklerk <justin@klerk.nl>
2021-03-04 10:53:43 +0100

    zo beter stephan ?

--- workshop.php
+++ workshop.php
@@ -4,6 +4,7 @@ $getworkshop = $pdo->prepare("SELECT * FROM `workshop` ");
 $getworkshop->execute();
 $arrayworkshop = $getworkshop->fetchAll();
 ?>
+
 <body class="bc-gray-black">
 <?php foreach ($arrayworkshop as $forworkshop): ?>
   <div class="parallax-halfheight d-flex align-items-center justify-content-center" style="background-image: url(<?php echo $forworkshop['workshop_img']; ?>);">
@@ -11,6 +12,7 @@ $arrayworkshop = $getworkshop->fetchAll();
             <h1 class="fos-2"><?php echo $forworkshop['workshop_title']; ?></h1>
         </div>
   </div>
+  <div class=" bc-gray-black" style="height: 2px;"></div>
 <?php endforeach; ?>
 </body>
 

125f54bc2ecc127b5436c0c734182b5bf40e4bed
StephanBear <Stephandebeer@xs4all.nl>
2021-03-04 13:13:40 +0100

    jarik enjoy

--- workshops.php
+++ workshops.php
@@ -26,14 +26,22 @@ if (empty($arrayworkshop['0'])) {
 
 <div class="c-white" style="margin: 8px;">
 <h1>
+    <iframe width="420" height="315" style="float: right;"
+            src="https://www.youtube.com/watch?v=w-HvWl89GGA">
+    </iframe>
     Ingredients:
    <br> human meat, Anime titties, Anime thighs, souls of the innocent, virgin sacrifice
 
     <br><br>
-    what u need: <br> een pan
+    what u need: <br> een pan, een koelkast, een fiets om stroom op te wekken, een palingworst
+
     <br><br>
     How to make blyat:
-   <br>cooking it ofc dumbass
+   <br>    Verwarm de oven voor tot 200 ºC. Verdeel over 4 royale vellen bakpapier of aluminiumfolie de tomaat en courgette. Leg er de schelvis op.
+    Roer de currypasta door de kokosmelk en schenk over de vis. Vouw het bakpapier of de aluminiumfolie tot pakketjes dicht. Bind ze eventueel vast met een stukje keukentouw of zet ze vast met (metalen!) paperclips.
+    Leg de pakketjes op een bakplaat en bak de vis in circa 20 minuten gaar in de voorverwarmde oven. Kook intussen de rijst volgens de aanwijzingen op de verpakking.
+    Leg de pakketjes op 4 borden. Vouw ze aan tafel open en bestrooi met de basilicumblaadjes. Serveer er de rijst en partjes limoen bij.
+    <img src="img/ja.jpg" style="float: right;">
 </h1>
 </div>
     </body>

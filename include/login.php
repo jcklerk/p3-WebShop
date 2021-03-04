6ded1957dbb7132594454fe2927a4e49c251193d
DrViggo <70940076+DrViggo@users.noreply.github.com>
2021-03-04 11:55:54 +0100

    klote login

--- include/login.php
+++ include/login.php
@@ -1,4 +1,11 @@
-<?php require __DIR__ . '/config.php' ?>
+<?php
+require __DIR__ . '/config.php';
+?>
+<head>
+  <link rel="stylesheet" href="../css/login.css">
+  <title>login</title>
+</head>
+<body>
     <div class="container">
         <div class="row">
             <div class="col-lg-3 col-md-2"></div>
@@ -12,14 +19,14 @@
 
                 <div class="col-lg-12 login-form">
                     <div class="col-lg-12 login-form">
-                        <form>
+                        <form method="post">
                             <div class="form-group">
                                 <label class="form-control-label">USERNAME</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" name="username" required>
                             </div>
                             <div class="form-group">
                                 <label class="form-control-label">PASSWORD</label>
-                                <input type="password" class="form-control" i>
+                                <input type="password" name="password" class="form-control" required>
                             </div>
 
                             <div class="col-lg-12 loginbttm">
@@ -27,7 +34,11 @@
                                     <!-- Error Message -->
                                 </div>
                                 <div class="col-lg-6 login-btm login-button">
-                                    <button type="submit" class="btn btn-outline-primary">LOGIN</button>
+                                    <input type="submit" class="btn btn-outline-primary" value="LOGIN" name="LOGIN">
+
+                                    <div class="col-lg-6 login-btm login-button">
+                                        <a href="#" class="btn btn-outline-primary">CREAT NEW USER</a>
+                                    </div>
                                 </div>
                             </div>
                         </form>
@@ -36,3 +47,22 @@
                 <div class="col-lg-3 col-md-2"></div>
             </div>
         </div>
+</body>
+<?php
+if (isset($_POST['LOGIN'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $query = $pdo->prepare("SELECT `user_id`,`username`,`password` FROM `user` WHERE `username` = :username");
+  $query->bindParam(':username', $username);
+  $query->execute();
+  $arraytest = $query->fetchAll();
+  print_r($arraytest);
+  echo $password;
+  if (password_verify($password, $arraytest['0']['password'])) {
+    header("Location: https://localhost/");
+  } else {
+    echo "fout!";
+  };
+};
+ ?>

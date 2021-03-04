38eb74e2a9b410bc85c6c00769244c9941342c4d
jcklerk <justin@klerk.nl>
2021-03-04 10:23:00 +0100

    working !

--- workshop.php
+++ workshop.php
@@ -7,11 +7,9 @@ $arrayworkshop = $getworkshop->fetchAll();
 <body class="bc-gray-black">
 <?php foreach ($arrayworkshop as $forworkshop): ?>
   <div class="parallax-halfheight d-flex align-items-center justify-content-center" style="background-image: url(<?php echo $forworkshop['workshop_img']; ?>);">
-      <a href="<?php echo $url;?>workshops.php/?workshops=<?php echo $forworkshop['workshop_id'];?>">
-        <div onclick='window.location.href = "<?php echo $url."workshops.php/?workshops=".$forworkshop['workshop_id;?>";' class="text-center">
+        <div onclick='window.location.href = "<?php echo $url."workshops.php/?workshops=".$forworkshop["workshop_id"];?>";' class="text-center">
             <h1 class="fos-2"><?php echo $forworkshop['workshop_title']; ?></h1>
         </div>
-      </a>
   </div>
 <?php endforeach; ?>
 </body>

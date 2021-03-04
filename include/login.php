28940823deadc4e0609d5e0b75850cbc4d7cda1c
jcklerk <justin@klerk.nl>
2021-03-04 13:30:31 +0100

    login

--- include/login.php
+++ include/login.php
@@ -1,53 +1,15 @@
 <?php
 require __DIR__ . '/config.php';
+if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
+  header("Location: ".$url."account.php");
+}
+$error = "";
 ?>
 <head>
-  <link rel="stylesheet" href="../css/login.css">
+  <link rel="stylesheet" href="<?php echo $url; ?>/css/login.css">
   <title>login</title>
 </head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-2"></div>
-            <div class="col-lg-6 col-md-8 login-box">
-                <div class="col-lg-12 login-key">
-                    <i class="fa fa-key" aria-hidden="true"></i>
-                </div>
-                <div class="col-lg-12 login-title">
-                    ADMIN PANEL
-                </div>
 
-                <div class="col-lg-12 login-form">
-                    <div class="col-lg-12 login-form">
-                        <form method="post">
-                            <div class="form-group">
-                                <label class="form-control-label">USERNAME</label>
-                                <input type="text" class="form-control" name="username" required>
-                            </div>
-                            <div class="form-group">
-                                <label class="form-control-label">PASSWORD</label>
-                                <input type="password" name="password" class="form-control" required>
-                            </div>
-
-                            <div class="col-lg-12 loginbttm">
-                                <div class="col-lg-6 login-btm login-text">
-                                    <!-- Error Message -->
-                                </div>
-                                <div class="col-lg-6 login-btm login-button">
-                                    <input type="submit" class="btn btn-outline-primary" value="LOGIN" name="LOGIN">
-
-                                    <div class="col-lg-6 login-btm login-button">
-                                        <a href="#" class="btn btn-outline-primary">CREAT NEW USER</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-2"></div>
-            </div>
-        </div>
-</body>
 <?php
 if (isset($_POST['LOGIN'])) {
   $username = $_POST['username'];
@@ -57,12 +19,18 @@ if (isset($_POST['LOGIN'])) {
   $query->bindParam(':username', $username);
   $query->execute();
   $arraytest = $query->fetchAll();
-  print_r($arraytest);
-  echo $password;
-  if (password_verify($password, $arraytest['0']['password'])) {
-    header("Location: https://localhost/");
+  if (!empty($arraytest)) {
+    if (password_verify($password, $arraytest['0']['password'])) {
+      $_SESSION['user_id'] = $arraytest['0']['user_id'];
+      $_SESSION['username'] = $arraytest['0']['username'];
+      header("Location: ".$url."account.php");
+    } else {
+      $error = "Wachtwoord niet Correct!";
+    }
   } else {
-    echo "fout!";
-  };
+    $error = "Account niet gevonden";
+  }
+
+  ;
 };
  ?>
---
+++ login.php
@@ -0,0 +1,46 @@
+<?php
+require __DIR__ . '/include/login.php';
+?>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-8 login-box">
+                <div class="col-lg-12 login-key">
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                </div>
+                <div class="col-lg-12 login-title">
+                    Login
+                </div>
+
+                <div class="col-lg-12 login-form">
+                    <div class="col-lg-12 login-form">
+                        <form method="post">
+                            <div class="form-group">
+                                <label class="form-control-label">USERNAME</label>
+                                <input type="text" class="form-control" name="username" required>
+                            </div>
+                            <div class="form-group">
+                                <label class="form-control-label">PASSWORD</label>
+                                <input type="password" name="password" class="form-control" required>
+                            </div>
+
+                            <div class="col-lg-12 loginbttm">
+                                <div class="col-lg-6 login-btm login-text">
+                                    <?php echo $error; ?>
+                                </div>
+                                <div class="col-lg-6 login-btm login-button">
+                                    <input type="submit" class="btn btn-outline-primary" value="LOGIN" name="LOGIN">
+
+                                    <div class="col-lg-6 login-btm login-button">
+                                        <a href="#" class="btn btn-outline-primary">CREAT NEW USER</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-2"></div>
+            </div>
+        </div>
+</body>

6d16581326b23f060371f9d9f1126ab82290b718
Julpower31 <jkragt@outlook.com>
2021-03-05 09:40:27 +0100

    ShopClass.php made

--- include/class/ShopClass.php
+++ include/class/ShopClass.php
@@ -4,28 +4,38 @@
  */
 class ShopClass
 {
+ public $cat;
+ private $dbClass;
 
-  function __construct(argument)
+  function __construct($cat)
   {
-    // code...
+    $this->cat = $cat;
+      $this->dbClass = new DBClass();
+  }
+  public function GetProductCat(){
+      $pdo = $this->dbClass->makeConnection();
+      $getproducts = $pdo->prepare("SELECT * FROM `product` WHERE `categorie` = :cat ");
+      $getproducts->bindParam(':cat', $this->cat);
+      $getproducts->execute();
+      $allproduct = $getproducts->fetchAll();
+      print_r($allproduct);
+      foreach ($allproduct as $x) { ?>
+          <div class="col">
+              <div class="card h-100 bc-gray-black ">
+                  <img src="img/<?php echo $x['1']?>" class="card-img-top" alt="...">
+                  <div class="card-body">
+                      <h5 class="card-title"><?php echo $x['2']; ?> </h5>
+                      <p class="card-text"><?php echo $x['3']; ?></p>
+                      <p class="card-text">€<?php echo $x['4']; ?></p>
+                  </div>
+              </div>
+          </div>
+      <?php }
   }
 }
 
 
-$getproducts = $pdo->prepare("SELECT * FROM `products`");
-$getproducts->execute();
-$allproduct = $getproducts->fetchAll();
 
 
-foreach ($allproduct as $x) { ?>
-        <div class="col">
-    <div class="card h-100 bc-gray-black ">
-<img src="img/<?php echo $x['1']?>" class="card-img-top" alt="...">
-<div class="card-body">
-<h5 class="card-title"><?php echo $x['2']; ?> </h5>
-<p class="card-text"><?php echo $x['3']; ?></p>
-<p class="card-text">€<?php echo $x['4']; ?></p>
-</div>
-    </div>
-        </div>
-<?php }
+
+
--- webshop.php
+++ webshop.php
@@ -1,50 +1,35 @@
 <?php
 require "include/nav.php";
-
+echo $_GET['cat'];
 
 ?>
 
-    <body class="bc-gray-black">
+<body class="bc-gray-black">
 
     <div class="container container-web">
 
         <div class="row row-web">
-            <div class="image-1-3 img-chinesefood d-flex align-items-center justify-content-center ">
-
-                <div class="text-center ts-1 ">
+            <div class="image-1-3 img-chinesefood d-flex align-items-center justify-content-center" onclick="window.location.href='<?php echo $url;?>webshop.php/?cat=0'">
+                <div class="text-center ts-1">
                     <h1 class="fos-2 op">Chinese food</h1>
-
                 </div>
             </div>
-
-
-            <div class="image-1-3 img-japanesefood d-flex align-items-center justify-content-center ">
-
+            <div class="image-1-3 img-japanesefood d-flex align-items-center justify-content-center" onclick="window.location.href='<?php echo $url;?>webshop.php/?cat=1'">
                 <div class="text-center ts-1" >
                     <h1 class="fos-2">Japanese food</h1>
-
                 </div>
             </div>
-
-            <div class="image-1-3 img-koreanfood d-flex align-items-center justify-content-center ">
-
+            <div class="image-1-3 img-koreanfood d-flex align-items-center justify-content-center" onclick="window.location.href='<?php echo $url;?>webshop.php/?cat=2'">
                 <div class="text-center ts-1">
                     <h1 class="fos-2">Korean food</h1>
-
                 </div>
             </div>
         </div>
     </div>
-
-
-            <div class="row row-web-prod row-cols-1 row-cols-md-4 g-4 c-red">
-
-
-    </div>
-
-
-
-
+    <div class="row row-web-prod row-cols-1 row-cols-md-4 g-4 c-red"></div>
+<?php
+(new ShopClass($_GET['cat']))->GetProductCat();
+?>
     </body>
 
 <?php

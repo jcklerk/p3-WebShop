0d080fd9f75afee5317adecc3d21990b4a95c5cf
jarik2498 <jscholta5104@student.landstede.nl>
2021-03-08 15:48:19 +0100

    iets

--- include/class/ShoppingCartClass.php
+++ include/class/ShoppingCartClass.php
@@ -13,17 +13,24 @@ class ShoppingCartClass
   public function Cart(){// loop door de array en laat de producten zien.
     $pdo = $this->dbClass->makeConnection();
 
-    foreach ($this->cart as $productid) {
+
+
+    foreach ($this->cart as $product) {
+        $getProducts = $pdo->prepare("SELECT * FROM `product` WHERE `product_nr` = :product");
+        $getProducts->bindParam(':product', $product);
+        $getProducts->execute();
+        $products = $getProducts->fetch();
+
       echo '
       <li class="list-group-item">
       <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="..." alt="...">
+            <img src="'. $products["img"] .'" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">'. $productid .'</h5>
+              <h5 class="card-title">'. $products["naam"] .'</h5>
               <p class="card-text"></p>
             </div>
           </div>

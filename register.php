9053f778cc442b0895fde2ce44817d489533abb2
DrViggo <70940076+DrViggo@users.noreply.github.com>
2021-03-08 08:49:14 +0100

    register

--- register.php
+++ register.php
@@ -13,8 +13,8 @@
 	 <form action="" method="" class="register-form">
       <div class="row">
            <div class="col-md-4 col-sm-4 col-lg-4">
-              <label for="firstName">NAME</label>
-               <input name="firstName" class="col-lg-6 login-btm login-button-twee" type="text">
+              <label for="username">USERNAME</label>
+               <input name="username" class="col-lg-6 login-btm login-button-twee" type="text" required>
            </div>
       </div>
       <div class="row">
@@ -22,23 +22,75 @@
       <div class="row">
            <div class="col-md-4 col-sm-4 col-lg-4">
               <label for="password">PASSWORD</label>
-               <input name="password" class="form-control" type="password">
+               <input name="password" class="form-control" type="password" required>
            </div>
       </div>
-      <hr>
+      <div class="row">
+           <div class="col-md-4 col-sm-4 col-lg-4">
+              <label for="voornaam">VOORNAAM</label>
+               <input name="voornaam" class="form-control" type="text" required>
+           </div>
+      </div>
+      <div class="row">
+           <div class="col-md-4 col-sm-4 col-lg-4">
+              <label for="tussenvoegsel">TUSSENVOEGSEL</label>
+               <input name="tussenvoegsel" class="form-control" type="text">
+      </div>
+      <div class="row">
+           <div class="col-md-4 col-sm-4 col-lg-4">
+              <label for="achternaam">ACHTERNAAM</label>
+               <input name="achternaam" class="form-control" type="text" required>
+           </div>
+      </div>
+      <div class="row">
+           <div class="col-md-4 col-sm-4 col-lg-4">
+              <label for="straatnaam">STRAATNAAM</label>
+               <input name="straatnaam" class="form-control" type="text" required>
+           </div>
+      </div>
+      <div class="row">
+           <div class="col-md-4 col-sm-4 col-lg-4">
+              <label for="huisnummer">HUISNUMMER</label>
+               <input name="huisnummer" class="form-control" type="text" required>
+           </div>
+      </div>
+      <div class="row">
+           <div class="col-md-4 col-sm-4 col-lg-4">
+              <label for="postcode">POSTCODE</label>
+               <input name="postcode" class="form-control" type="text" required>
+           </div>
+      </div>
+      <div class="row">
+           <div class="col-md-4 col-sm-4 col-lg-4">
+              <label for="woonplaats">WOONPLAATS</label>
+               <input name="woonplaats" class="form-control" type="text" required>
+           </div>
+      </div>
       <div class="row">
            <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
            <button class="btn btn-default regbutton">Register</button>
-
           </div>
           <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
-           <button class="btn btn-default logbutton">Sign up</button>
+           <a href="login.php" class="btn btn-default logbutton">Sign up</a>
           </div>
-
       </div>
-
-</div>
-
     </form>
-
+</div>
 </body>
+<?php
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['voornaam']) && isset($_POST['achternaam']) && isset($_POST['straatnaam']) && isset($_POST['huisnummer']) && isset($_POST['postcode']) && isset($_POST['woonplaats'])) {
+    $q = $pdo->prepare('INSERT INTO `user` (`user_id`, `username`, `password`, `voornaam`, `tussenvoegsel`, `achternaam`, `straatnaam`, `huisnummer`, `postcode`, `woonplaats`) VALUES (NULL, :ui, :u, :p, :v, :t, :a, :s, :h, :pc, :w)');
+    $q->bindParam(':ui', $_SESSION['user_id']);
+    $q->bindParam(':u', $_POST['username']);
+    $q->bindParam(':p', $_POST['password']);
+    $q->bindParam(':v', $_POST['vornaam']);
+    $q->bindParam(':t', $_POST['tussenvoegsel']);
+    $q->bindParam(':a', $_POST['achternaam']);
+    $q->bindParam(':s', $_POST['straatnaam']);
+    $q->bindParam(':h', $_POST['huisnummer']);
+    $q->bindParam(':pc', $_POST['postcode']);
+    $q->bindParam(':w', $_POST['woonplaats']);
+    $q->execute();
+    header('Location: http://localhost/gebruiker/');
+}
+?>

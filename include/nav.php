51c845ddcaf7bb3b9af3c7e0a90b120bd1d2cba2
StephanBear <Stephandebeer@xs4all.nl>
2021-03-08 15:44:14 +0100

    julain of wie m had gemaakt fix je shit en maak t dropdown

--- include/nav.php
+++ include/nav.php
@@ -8,6 +8,7 @@ require __DIR__ . '/config.php';
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <!--dit is je kut probleem en idk what fix-->
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">

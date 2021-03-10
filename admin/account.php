b46d1dd14a6a7b567b61f473de38014a3c7dac89
jarik2498 <jscholta5104@student.landstede.nl>
2021-03-10 10:43:19 +0100

    admin page

--- admin/account.php
+++ admin/account.php
@@ -1,1 +1,39 @@
 <?php
+
+//require __DIR__ . '/User.php';
+//$user = new User();
+//$user->login();
+
+require "include/nav.php";
+require "include/class/FactuurClass.php";
+
+?>
+
+    <body class="bc-gray-black">
+
+    <table class="table table-edit c-red" style="">
+        <tbody>
+        <tr class="c-border">
+            <th scope="col" >Oder nummer</th>
+            <th scope="col">product</th>
+            <th scope="col">datum</th>
+            <th scope="col">Status</th>
+            <th scope="col">totaal</th>
+        </tr>
+
+        <?php (new FactuurClass($_SESSION['user_id']))->GetFacatuur(); ?>
+
+        </tbody>
+    </table>
+
+
+
+
+
+    </body>
+
+
+
+<?php
+require "include/footer.php";
+?>

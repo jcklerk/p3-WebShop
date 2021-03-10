4009da2d22bd30f02965cf396cb5115b5ff7af96
jarik2498 <jscholta5104@student.landstede.nl>
2021-03-10 10:47:31 +0100

    admin class

---
+++ include/class/AdminClass.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *
+ */
+class AdminClass
+{
+  private $dbClass;
+  private $pdo;
+  public $username;
+  public $error;
+  private $password;
+
+  function __construct($username, $password)
+  {
+    $this->dbClass = new DBClass();
+    $this->username = $username;
+    $this->password = $password;
+  }
+  public function ProductInsert(){
+    $pdo = $this->dbClass->makeConnection();
+    $login = $pdo->prepare("SELECT `user_id`,`username`,`password` FROM `user` WHERE `username` = :username");
+    $login->bindParam(':username', $this->username);
+    $login->execute();
+    $arraylogin = $login->fetch();
+    if (!empty($arraylogin)) {
+      if (password_verify($this->password, $arraylogin['password'])) {
+        $_SESSION['user_id'] = $arraylogin['user_id'];
+        $_SESSION['username'] = $arraylogin['username'];
+        echo '<script type="text/javascript"> window.location.href = "'.$url.'account.php"</script>';
+      } else {
+        $error = "Wachtwoord niet Correct!";
+      }
+    } else {
+      $error = "Account niet gevonden";
+    }
+    return $error;
+  }
+}
+
+?>

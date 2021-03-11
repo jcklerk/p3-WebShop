1b63926a2150e49af294cc743ca52264bd2bb808
jarik2498 <jscholta5104@student.landstede.nl>
2021-03-11 13:42:10 +0100

    Admin workshop aangepast

--- include/class/AdminWorkshopClass.php
+++ include/class/AdminWorkshopClass.php
@@ -11,12 +11,10 @@ class AdminClass
   private $workshop_img;
   private $video;
   private $img;
-  private $ingredienten;
-  private $benodigdheden;
-  private $maken;
 
 
-  function __construct($workshop_title, $workshop_img, $video, $img, $ingredienten, $benodigdheden, $maken)
+
+  function __construct($workshop_title, $workshop_img, $video, $img)
   {
     $this->dbClass = new DBClass();
 
@@ -24,9 +22,6 @@ class AdminClass
     $this->workshop_img = $workshop_img;
     $this->video = $video;
     $this->img = $img;
-    $this->ingredienten = $ingredienten;
-    $this->benodigdheden = $benodigdheden;
-    $this->maken = $maken;
   }
 
 

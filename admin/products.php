cc17e230d799d3fe5c0b9607f7908e494729dfb3
StephanBear <Stephandebeer@xs4all.nl>
2021-03-18 10:44:20 +0100

    doggo

--- admin/products.php
+++ admin/products.php
@@ -1,7 +1,6 @@
-hier ga ik dan een lijst met alle producten doen
-
-product 1
-product 2
-product 3
-product 4
-product 5
+<div class="card" style="width: 17rem; border: 1px solid black">
+    <img src="../img/ja.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <p class="card-text"><a href="yeet" style="margin-right: 200px; margin-left: 10px">edit</a><a href="yeett">add</a> </p>
+    </div>
+</div>

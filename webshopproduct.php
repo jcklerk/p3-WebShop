2837f560c67c4f63c642e7d05f6a2941e4e0e487
jkragt <jkragt4711@student.landstede.nl>
2021-03-24 14:59:45 +0100

    lalala

--- webshopproduct.php
+++ webshopproduct.php
@@ -3,17 +3,5 @@
 require 'include/nav.php';
 require "include/class/ShopProductClass.php";
 (new ShopProductClass($_GET['product_nr']))->Product();
-?>
-
-
-
-
-
-
-
-
-
-
-<?php
 require 'include/footer.php';
 ?>

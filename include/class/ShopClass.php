124ca0f66e6923e4b45b8497cb0b2e9570d7649a
jcklerk <justin@klerk.nl>
2021-03-25 10:36:23 +0100

    get all products fix

--- include/class/ShopClass.php
+++ include/class/ShopClass.php
@@ -43,8 +43,8 @@ class ShopClass
   }
   public function GetProductall(){
       $pdo = $this->dbClass->makeConnection();
-      $getproducts = $pdo->prepare("SELECT * FROM `product` WHERE `categorie` = :cat ");
-      $getproducts->bindParam(':cat', $this->cat);
+      $getproducts = $pdo->prepare("SELECT * FROM `product`INNER JOIN `taal_product` ON product.product_nr = taal_product.product_nr WHERE  `taal_id` = :taal_id");
+      $getproducts->bindParam(':taal_id', $_SESSION['lang_id']);
       $getproducts->execute();
       $allproduct = $getproducts->fetchAll();
       foreach ($allproduct as $x) { ?>

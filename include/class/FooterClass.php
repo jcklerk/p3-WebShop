0b40318ee44afb94bca746636a0082add5e387a6
Julpower31 <jkragt@outlook.com>
2021-03-25 08:56:21 +0100

    Footer lang fix

--- include/class/FooterClass.php
+++ include/class/FooterClass.php
@@ -9,12 +9,13 @@ class FooterClass
     {
         $this->dbClass = new DBClass();
     }
-    public function GetProductCat(){
+    public function Getfooter(){
         $pdo = $this->dbClass->makeConnection();
-        $getproducts = $pdo->prepare("SELECT * FROM `taal_footer` WHERE `taal_id` = 1");
-        $getproducts->execute();
-        $catproduct = $getproducts->fetchAll();
-        foreach ($catproduct as $x) { ?>
+        $getfooter = $pdo->prepare("SELECT * FROM `taal_footer` WHERE `taal_id` = :taal_id");
+        $getfooter->bindParam(':taal_id', $_SESSION['lang_id']);
+        $getfooter->execute();
+        $footer = $getfooter->fetchAll();
+        foreach ($footer as $x) { ?>
             <footer class="text-center bc-red c-yellow">
                 <div class="container p-4">
                     <div class="row">
@@ -32,27 +33,5 @@ class FooterClass
             </footer>
         <?php }
     }
-    public function GetProductall(){
-        $pdo = $this->dbClass->makeConnection();
-        $getproducts = $pdo->prepare("SELECT * FROM `taal_footer` WHERE `taal_id` = 2");
-        $getproducts->execute();
-        $allproduct = $getproducts->fetchAll();
-        foreach ($allproduct as $x) { ?>
-            <footer class="text-center bc-red c-yellow">
-                <div class="container p-4">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-12 mb-4 mb-md-0 fw-bold">
-                            <p class="c-yellow">
-                                Contact: <br> Mail: <br> <?php echo $x['phone'] ?> nr: <br> <?php echo $x['adres'] ?>:
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="text-center p-3 fw-bold" style=" background-color: rgba(151, 0, 0, 0.5);">
-                    © 2021 Copyright:
-                    <a>SD20-1</a>
-                </div>
-            </footer>
-        <?php }
-    }
+
 }
--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,3 @@
 <?php
 require 'class/FooterClass.php';
-if ($_SESSION['lang_id'] == 1){
-    (new FooterClass())->GetProductCat();
-}elseif($_SESSION['lang_id'] == 2){
-    (new FooterClass())->GetProductall();
-}
+(new FooterClass())->Getfooter();

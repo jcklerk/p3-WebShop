9df3f83f29363db40257d78b81656971ec4116be
jcklerk <justin@klerk.nl>
2021-03-30 09:28:02 +0200

    test

--- admin/addproducts.php
+++ admin/addproducts.php
@@ -27,9 +27,7 @@ if (isset($_POST['Webshop_img']) && isset($_POST['Webshop_Cat']) && isset($_POST
 
 ?>
 <title>Add Product - Wok & Roll</title>
-<br><br><br><br>
-<br>
-<h1>Add Product</h1>
+<h1 class="tm-1">Add Product</h1>
 <br>
 <body class="d-flex flex-column h-100" style="text-align: center">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>

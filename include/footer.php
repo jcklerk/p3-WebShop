009ff3a3a223f14d6ca53c4307c495f1ca3a6d77
jcklerk <justin@klerk.nl>
2021-04-07 11:47:26 +0200

    footer link to faq fix

--- include/footer.php
+++ include/footer.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="mb-4 mb-md-0 fw-bold"> <!-- removed from class: col-lg-6 col-md-12 -->
               <p>
-                <h4><a class="c-yellow" href="<?php echo $_SESSION['url']; ?>FAQ.php">FAQ</a></h4>
+                <h4><a class="c-yellow" href="<?php echo $url; ?>FAQ.php">FAQ</a></h4>
               </p>
                 <p class="c-yellow">
                     <h4> Contact:</h4> Mail: example@example.com <br> <?php echo $arraytekst['phone'] ?> nr: +31 123456789 <br> <?php echo $arraytekst['adres'] ?>: Nederland, Harderwijk, Westeinde 33

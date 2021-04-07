9cf621f1a9daf84076403a418282cc85eb3d2cc2
jcklerk <justin@klerk.nl>
2021-04-07 15:10:55 +0200

    fix lang

--- include/class/ShopProductClass.php
+++ include/class/ShopProductClass.php
@@ -13,7 +13,7 @@ class ShopProductClass
     }
     public function Product(){
         $pdo = $this->dbClass->makeConnection();
-        $getproducts = $pdo->prepare("SELECT * FROM `product`INNER JOIN `taal_product` ON product.product_nr = taal_product.product_nr LEFT JOIN `taal_shop` ON taal_shop.taal_id = taal_product.taal_id   WHERE product.product_nr = :product_nr AND taal_product.taal_id = :taal_id");
+        $getproducts = $pdo->prepare("SELECT * FROM `product`INNER JOIN `taal_product` ON product.product_nr = taal_product.product_nr   WHERE product.product_nr = :product_nr AND taal_product.taal_id = :taal_id");
         $getproducts->bindParam(':product_nr', $this->product);
         $getproducts->bindParam(':taal_id', $_SESSION['lang_id']);
         $getproducts->execute();
@@ -33,7 +33,7 @@ class ShopProductClass
                             <p class="card-text c-red">
                             <form method="post">
                                 <button type="submit" class="btn btn-primary btn-lg" name="product_nr" value="<?php echo $x['product_nr'];?>">
-                                    <?php echo $x['pay']?>
+                                    <?php echo $GLOBALS['arraytekst']['pay']?>
                                 </button>
                             </form>
                             </p>
@@ -53,16 +53,10 @@ class ShopProductClass
                 $_SESSION['cart'][$_POST['product_nr']]['Product'] = $_POST['product_nr'];
                 $_SESSION['cart'][$_POST['product_nr']]['Aantal'] = 1;
             }
-                $pdo = $this->dbClass->makeConnection();
-                $gettaalproducts = $pdo->prepare("SELECT `add_to_card_melding` FROM `taal_shop` WHERE `taal_id` = :taal_id ");
-                $gettaalproducts->bindParam(':taal_id', $_SESSION['lang_id']);
-                $gettaalproducts->execute();
-                $taalaantalproduct = $gettaalproducts->fetchAll();
-                foreach ($taalaantalproduct as $a) {
                     ?> <div class="alert alert-success" role="alert">
-                        <p><?php echo $a['add_to_card_melding'].' '.$_SESSION['cart'][$_POST['product_nr']]['Aantal']?></p>
+                        <p><?php echo $GLOBALS['arraytekst']['add_to_cart_melding'].' '.$_SESSION['cart'][$_POST['product_nr']]['Aantal']?></p>
                     </div>
-                <?php }
+                <?php
             }
         }
     }

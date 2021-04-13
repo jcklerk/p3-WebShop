247e810066a9e5f9c21046a4862bf9efc90e8520
jcklerk <justin@klerk.nl>
2021-04-13 12:01:46 +0200

    test

--- include/config.php
+++ include/config.php
@@ -46,6 +46,7 @@ echo '<link rel="icon"
       href="'.$url.'/img/logo.png">';
 
 ?>
+<!DOCTYPE html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">

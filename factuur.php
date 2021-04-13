e993b9d2f423b4805c75b36756f029a3bf2ce349
jcklerk <justin@klerk.nl>
2021-04-13 08:46:42 +0200

    test

--- factuur.php
+++ factuur.php
@@ -69,18 +69,18 @@ $totaal = array();
   <table class="table">
       <tbody>
       <tr class="c-border">
-          <th scope="col" ><?php echo $arraytekst['aantal']; ?> </th>
           <th scope="col"><?php echo $arraytekst['product']; ?></th>
           <th scope="col"><?php echo $arraytekst['prijs']; ?></th>
+          <th scope="col" ><?php echo $arraytekst['aantal']; ?> </th>
           <th scope="col"><?php echo $arraytekst['totaal']; ?></th>
           <th scope="col">BTW</th>
       </tr>
 
       <?php foreach ($factuur['factuur'] as $product): ?>
         <tr>
-        <th scope="col" ><?php echo $product['product_aantal']; ?> </th>
         <th scope="col"><?php echo $product['naam']; ?></th>
         <th scope="col"><?php echo '€ '.$product['prijs']; ?></th>
+        <th scope="col" ><?php echo $product['product_aantal']; ?> </th>
         <th scope="col"><?php echo '€ '.$product['prijs'] * $product['product_aantal']; $totaal[] = $product['prijs'] * $product['product_aantal'];?></th>
         <th scope="col"><?php echo $product['btw'].'%'; ?></th>
         </tr>
@@ -93,7 +93,7 @@ $totaal = array();
     </div>
     <div class="col">
     </div>
-    <div class="col">
+    <div class="col-6">
        <h6><?php echo $arraytekst['verzend'].': € '.$factuur['factuur'][0]['verzend_kosten'];  $totaal[] = $factuur['factuur'][0]['verzend_kosten']; ?></h6>
       <h6><?php echo $arraytekst['totaal_1'].' € '.array_sum($totaal) ; ?></h6>
     </div>
